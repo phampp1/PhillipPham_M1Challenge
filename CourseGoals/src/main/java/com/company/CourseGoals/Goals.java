6a48f50ead05042b14f3b97773c2470c3aac0c96
Phillip Pham <phampp1@uci.edu>
2022-01-23 17:04:31 -0800

    Module Challenge 1: Goals

---
+++ CourseGoals/src/main/java/com/company/CourseGoals/Goals.java
@@ -0,0 +1,11 @@
+package com.company.CourseGoals;
+
+public class Goals {
+    public static void main(String[] args) {
+        System.out.println(
+                "Below are my personal three goals for this course:" + "\n"
+                        + "1. Be able to write and read code in the Java language" + "\n"
+                        + "2. Learn how to collaborate with other developers when building applications" + "\n"
+                        + "3. Learn about the foundations of Object-Oriented-Programming.");
+    }
+}
